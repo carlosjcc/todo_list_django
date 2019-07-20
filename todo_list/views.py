faa24b516c0470dca7f2a53426f3780d0a70f779
carlosjcc <carloscorralesch@gmail.com>
2019-07-21 00:24:35 +1000

    adds messagin functionality

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 from .models import List
 from .forms import ListForm
@@ -11,6 +12,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, ('Item Has Been Added To List!'))
             return render(request, 'home.html', {'all_items': all_items})
     else:
         all_items = List.objects.all
