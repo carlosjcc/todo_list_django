e05e89387e644d4ea78bf5828d5fd4c0614cb379
carlosjcc <carloscorralesch@gmail.com>
2019-07-21 22:47:56 +1000

    adds functionality to the checkbox

--- todo_list/views.py
+++ todo_list/views.py
@@ -30,16 +30,18 @@ def delete(request, list_id):
     return redirect('home')
 
 def cross_off(request, list_id):
-    item = List.objects.get(pk=list_id)
-    item.completed = True
-    item.save()
-    return redirect('home')
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        item.completed = True
+        item.save()
+        return redirect('home')
 
 def uncross(request, list_id):
-    item = List.objects.get(pk=list_id)
-    item.completed = False
-    item.save()
-    return redirect('home')
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        item.completed = False
+        item.save()
+        return redirect('home')
 
 def edit(request, list_id):
     if request.method == 'POST':
